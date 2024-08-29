b40bf42a8fee3935baddf49ca3fff4f9db0d6fd3
weesleycabral <weesleylima@hotmail.com>
2024-08-29 12:16:26 -0300

    feat: Add sign-up page and orders page to router configuration

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,26 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Dashboard } from './pages/app/dashboard';
-import { SignIn } from './pages/auth/sign-in';
-import { AppLayout } from './pages/_layouts/app';
-import { AuthLayout } from './pages/_layouts/auth';
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }]
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
-    path: '/',
+    path: "/",
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }]
-  }
-])
+    children: [
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
+  },
+]);
