b99b2c5a77c63350f63318ffb0e8945f70f1f380
weesleycabral <weesleylima@hotmail.com>
2024-08-29 12:16:13 -0300

    chore: Add theme provider and toggle component for dynamic theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { RouterProvider } from 'react-router-dom'
-import './global.css'
-import { router } from './routes'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from "react-router-dom";
+import "./global.css";
+import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
-  )
+  );
 }
-
-
